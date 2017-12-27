4c608e7f0b864f07653a1befa32b64bdc3b80530
madeindjs <contact@rousseau-alexandre.fr>
2017-12-27 13:24:56 +0100

    Create custom list view

---
+++ app/src/main/java/com/rousseau_alexandre/raspberrycook/Recipe.java
@@ -0,0 +1,13 @@
+package com.rousseau_alexandre.raspberrycook;
+
+/**
+ * Created by arousseau on 27/12/17.
+ */
+
+public class Recipe {
+    public String name;
+
+    public Recipe(String _name){
+        name = _name;
+    }
+}
