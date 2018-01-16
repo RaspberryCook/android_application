e63843d82e083d3036f51ed836cb69b20df5ea02
madeindjs <contact@rousseau-alexandre.fr>
2018-01-16 13:03:13 +0100

    Move delete floating button to menu

--- app/src/main/java/com/rousseau_alexandre/raspberrycook/RecipeActivity.java
+++ app/src/main/java/com/rousseau_alexandre/raspberrycook/RecipeActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -31,8 +33,7 @@ public class RecipeActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                recipe.delete(RecipeActivity.this);
-                finish();
+
             }
         });
     }
@@ -44,6 +45,30 @@ public class RecipeActivity extends AppCompatActivity {
     private Recipe getRecipe(){
         Intent intent = getIntent();
         return (Recipe) intent.getSerializableExtra(MainActivity.EXTRA_RECIPE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.recipe, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.editMenuItem:
+                // newGame();
+                return true;
+            case R.id.deleteMenuItem:
+                getRecipe().delete(RecipeActivity.this);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
